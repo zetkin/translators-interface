3bcb43d0848a3bd21f48ab80fee41af6d85a91e2
River H <eribloodlust@grrlz.net>
2021-05-18 19:27:18 +0100

    add http to env files, take out of url in fetch

--- frontend/src/api/projects.ts
+++ frontend/src/api/projects.ts
@@ -2,9 +2,11 @@ import { Project } from '../global.types'
 import fetchWrapper from './fetchWrapper'
 
 export const getProjects = async (): Promise<Project[]> => {
-  return fetchWrapper({ url: `http://${process.env.NEXT_PUBLIC_API_HOST}/projects/` })
+  return fetchWrapper({ url: `${process.env.NEXT_PUBLIC_API_HOST}/projects/` })
 }
 
 export const getProject = async (projectId): Promise<Project> => {
-  return fetchWrapper({ url: `http://${process.env.NEXT_PUBLIC_API_HOST}/projects/${projectId}/` })
+  return fetchWrapper({
+    url: `${process.env.NEXT_PUBLIC_API_HOST}/projects/${projectId}/`,
+  })
 }
