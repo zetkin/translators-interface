f1eb614b0ad05708f57060126b4154081c2ca8ca
River H <eribloodlust@grrlz.net>
2021-06-07 11:46:33 +0100

    set email button!

--- frontend/src/components/EmailField.tsx
+++ frontend/src/components/EmailField.tsx
@@ -1,33 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fade } from '@material-ui/core/styles'
 
-import { InputBase } from '@material-ui/core'
+import { InputBase, Button, Tooltip } from '@material-ui/core'
 
 import theme from '../../styles/theme'
 
 import { AUTHOR_NAME_LOCAL_STORAGE_KEY } from '../constants'
 import useLocalStorage from '../hooks/useLocalStorage'
 
+const isEmail = (text: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(text).toLowerCase())
+}
+
 const EmailField = () => {
-  const [authorName, setAuthorName] = useLocalStorage<string>(
+  const [authorEmail, setAuthorEmail] = useLocalStorage<string>(
     AUTHOR_NAME_LOCAL_STORAGE_KEY
   )
+  const [value, setValue] = useState<string>(authorEmail)
+  const [error, setError] = useState<boolean>()
+
+  const handleSave = () => {
+    if (!error) {
+      setAuthorEmail(value)
+    }
+  }
 
   return (
-    <div
-      style={{
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        color: theme.palette.common.white,
-        borderRadius: 4,
-        padding: theme.spacing(0.25, 2),
-      }}
-    >
-      <InputBase
-        placeholder="Your name..."
-        style={{ color: 'inherit' }}
-        value={authorName}
-        onChange={(e) => setAuthorName(e.target.value)}
-      />
+    <div style={{ display: 'flex' }}>
+      <div
+        style={{
+          backgroundColor: fade(theme.palette.common.white, 0.15),
+          color: theme.palette.common.white,
+          borderRadius: 4,
+          padding: theme.spacing(0.25, 2),
+        }}
+      >
+        <InputBase
+          placeholder="Your email..."
+          style={{ color: 'inherit' }}
+          value={value}
+          onChange={(e) => {
+            // Set current value
+            setValue(e.target.value)
+            // Check if error
+            setError(!isEmail(e.target.value))
+          }}
+        />
+      </div>
+      {
+        // If current value different than saved value
+        value != authorEmail && (
+          <Tooltip
+            title="Email address not valid"
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            arrow
+            open={error}
+          >
+            <Button
+              disabled={error}
+              style={{
+                backgroundColor: fade(theme.palette.common.white, 0.15),
+                color: theme.palette.common.white,
+                borderRadius: 4,
+                padding: theme.spacing(0.25, 2),
+                marginLeft: '5px',
+              }}
+              variant="contained"
+              disableElevation
+              onClick={handleSave}
+            >
+              Save
+            </Button>
+          </Tooltip>
+        )
+      }
     </div>
   )
 }
