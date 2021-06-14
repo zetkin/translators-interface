39cad4df90f3f161eca123e7f04962fd930888cf
River H <eribloodlust@grrlz.net>
2021-06-14 13:46:17 +0100

    check production explicitly, consistent with frontend

--- backend/run.sh
+++ backend/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ $ENVIRONMENT == 'development' ]]; then
-    python manage.py runserver 0.0.0.0:8000
-else
+if [[ $ENVIRONMENT == 'production' ]]; 
+then
     gunicorn -b 0.0.0.0:8000 app.wsgi:application
+else
+    python manage.py runserver 0.0.0.0:8000
 fi
