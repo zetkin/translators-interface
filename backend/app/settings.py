663117ac7633d78e2f0fc01001a177c8764ba0fc
River H <eribloodlust@grrlz.net>
2021-06-14 14:18:56 +0100

    add secret key to env config

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+pxk2@z*pgj7c8n+a7e(3cb=o%2j43#od9ald#s=jwa2nxzqpq"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
